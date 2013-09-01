ca44078c0e7de6976483fd5e2406363d82f04d56
Thomas Davis <sunsetbrew@sunsetbrew.com>
2013-09-01 12:00:27 -0400

    Updated buildroot version

--- contrib/buildroot/civetweb.mk
+++ contrib/buildroot/civetweb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CIVETWEB_VERSION = 1.3
+CIVETWEB_VERSION = 1.4
 CIVETWEB_SITE = http://github.com/sunsetbrew/civetweb/tarball/v$(CIVETWEB_VERSION)
 CIVETWEB_LICENSE = MIT
 CIVETWEB_LICENSE_FILES = LICENSE.md
