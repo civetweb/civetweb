aa2cded31fcfc37c22c23747332af567822a80a1
bel <bel2125@gmail.com>
2013-12-10 20:41:32 +0100

    New + better websocket example

--- examples/websocket/WebSockCallbacks.h
+++ examples/websocket/WebSockCallbacks.h
@@ -9,6 +9,10 @@ extern "C" {
 #endif
 
 void websock_init_lib(void);
+void websock_exit_lib(void);
+
+void websock_send_broadcast(const char * data, int data_len);
+
 void websocket_ready_handler(struct mg_connection *conn);
 int websocket_data_handler(struct mg_connection *conn, int flags, char *data, size_t data_len);
 void connection_close_handler(struct mg_connection *conn);
