d4b3569edbf055616cc311f08ae4e6ee2170c286
bel2125 <bel2125@gmail.com>
2015-12-30 23:27:16 +0100

    Update coverity scan script

--- resources/coverity_check.sh
+++ resources/coverity_check.sh
@@ -5,7 +5,7 @@ if [ "$?" = "0" ]; then
 	echo "Building files for coverity check ..."
 else
 	echo "Run this script from the root directory of project!" 1>&2
-	echo "username@hostname:/somewhere/civetweb$ resources/coverity_check.sh" 1>&2
+	echo "username@hostname:/somewhere/civetweb$ ./resources/coverity_check.sh" 1>&2
 	exit 1
 fi
 
@@ -14,15 +14,18 @@ make clean
 
 ../cov-analysis-linux64-7.6.0/bin/cov-build  --dir cov-int make WITH_IPV6=1 WITH_WEBSOCKET=1 WITH_LUA_SHARED=1
 
-rm coverity_check.tgz
-tar czvf coverity_check.tgz cov-int
+rm civetweb_coverity_check.tgz
+tar czvf civetweb_coverity_check.tgz cov-int
 
-ls coverity_check.tgz
+echo
+ls -la civetweb_coverity_check.tgz
 
 if [ "$?" = "0" ]; then
 	echo "... done"
+        echo
 else
-	echo "No coverity_check.tgz file" 1>&2
+	echo "No civetweb_coverity_check.tgz file" 1>&2
+        echo
 	exit 1
 fi
 
