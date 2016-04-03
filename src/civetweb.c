c176426c9f70bb13afbde9af5aa0dbcd54a1db2f
Kevin Wojniak <kainjow@users.noreply.github.com>
2016-04-02 17:01:30 -0700

    Fix "cast from 'const struct in_addr *' to 'unsigned int *' drops const qualifier"

--- src/civetweb.c
+++ src/civetweb.c
@@ -9101,7 +9101,7 @@ get_remote_ip(const struct mg_connection *conn)
 	if (!conn) {
 		return 0;
 	}
-	return ntohl(*(uint32_t *)&conn->client.rsa.sin.sin_addr);
+	return ntohl(*(const uint32_t *)&conn->client.rsa.sin.sin_addr);
 }
 
 
