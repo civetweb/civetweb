72e735ba6c9f760483c20ff9c18125566a23027b
xtne6f <xt4ubq@gmail.com>
2016-06-02 19:02:46 +0900

    Revert "Win64 builds should use OpenSSL 64 bit as default"
    This reverts commit a41e091973c6a60958f1178e4f71910c991b76b3.

--- src/civetweb.c
+++ src/civetweb.c
@@ -267,15 +267,8 @@ typedef long off_t;
 
 #define ERRNO ((int)(GetLastError()))
 #define NO_SOCKLEN_T
-
-#if defined(_WIN64) || defined(__MINGW64__)
-#define SSL_LIB "ssleay64.dll"
-#define CRYPTO_LIB "libeay64.dll"
-#else
 #define SSL_LIB "ssleay32.dll"
 #define CRYPTO_LIB "libeay32.dll"
-#endif
-
 #define O_NONBLOCK (0)
 #ifndef W_OK
 #define W_OK (2) /* http://msdn.microsoft.com/en-us/library/1w06ktdy.aspx */
