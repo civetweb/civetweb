aec25534c2d1858c8934ff56dcb668c655d5c235
bel <bel2125@gmail.com>
2016-06-12 23:24:57 +0200

    If NO_SSL is defined, dlopen is not required

--- src/civetweb.c
+++ src/civetweb.c
@@ -3375,7 +3375,8 @@ mg_join_thread(pthread_t threadid)
 	return result;
 }
 
-#if !defined(NO_SSL_DL)
+#if !defined(NO_SSL_DL) && !defined(NO_SSL)
+/* If SSL is loaded dynamically, dlopen/dlclose is required. */
 /* Create substitutes for POSIX functions in Win32. */
 
 #if defined(__MINGW32__)
