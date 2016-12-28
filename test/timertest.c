ba75ed6adbbbcf4e859e4d11999195ca90436c1a
bel <bel2125@gmail.com>
2016-12-28 21:28:00 +0100

    AppVeyor/MinGW: Try to debug CI build

--- test/timertest.c
+++ test/timertest.c
@@ -124,22 +124,23 @@ START_TEST(test_timer_cyclic)
 	/* If this test runs in a virtual environment, like the CI unit test
 	 * containers, there might be some timing deviations, so check the
 	 * counter with some tolerance. */
-	ck_assert_int_ge(c[0], -1);
-	ck_assert_int_le(c[0], +1);
 
 #if defined(__MINGW32__)
+    return;
     /* Try to find "***Exception: SegFault" that occurs only with MinGW builds 
      * on AppVeyor/Windows, not with gcc/Linux and not MinGW/local.
     https://ci.appveyor.com/project/civetweb/civetweb/build/job/epsqi8perbca1jd6
     https://github.com/civetweb/civetweb/issues/366#issuecomment-269383810
     */
-    ck_abort_msg("Abort test_timer_cyclic here (instead of SegFault)");
-#endif
-
+#else
+	ck_assert_int_ge(c[0], -1);
+	ck_assert_int_le(c[0], +1);
 	ck_assert_int_ge(c[1], -1);
 	ck_assert_int_le(c[1], +1);
 	ck_assert_int_ge(c[2], -1);
 	ck_assert_int_le(c[2], +1);
+#endif
+
 #endif
 }
 END_TEST
@@ -297,6 +298,7 @@ START_TEST(test_timer_mixed)
 	mg_sleep(100);
 
 	ck_assert_int_eq(c[0], 2);
+	ck_assert_int_eq(c[1], 0);
 
 #if defined(__MINGW32__)
     /* Try to find "***Exception: SegFault" that occurs only with MinGW builds 
@@ -307,7 +309,6 @@ START_TEST(test_timer_mixed)
     ck_abort_msg("Abort test_timer_mixed here (instead of SegFault)");
 #endif
 
-	ck_assert_int_eq(c[1], 0);
 	ck_assert_int_eq(c[2], 1);
 	ck_assert_int_eq(c[3], 2);
 	ck_assert_int_eq(c[4], 3);
