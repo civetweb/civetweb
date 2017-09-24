242ba025ac82dcbaa3e2ff6a7ec504883404d43b
bel2125 <bel2125@gmail.com>
2017-09-24 23:46:16 +0200

    Restore AppVeyor build

--- test/private.c
+++ test/private.c
@@ -494,6 +494,13 @@ START_TEST(test_mg_vsnprintf)
 	int is_trunc;
 
 #if defined(_WIN32)
+	/* If the string is truncated, mg_snprintf calls mg_cry.
+	 * If DEBUG is defined, mg_cry calls DEBUG_TRACE.
+	 * In DEBUG_TRACE_FUNC, flockfile(stdout) is called.
+	 * For Windows, flockfile/funlockfile calls Enter-/
+	 * LeaveCriticalSection(&global_log_file_lock).
+	 * So, we need to initialize global_log_file_lock:
+	 */
 	InitializeCriticalSection(&global_log_file_lock);
 #endif
 
