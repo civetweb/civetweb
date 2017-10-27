9e56b98611e896254eb032becb4690fa52fa86ee
bel2125 <bel2125@gmail.com>
2017-10-27 22:28:57 +0200

    Fix client - Support multiple domains (Step 5/?)

--- src/civetweb.c
+++ src/civetweb.c
@@ -15132,7 +15132,7 @@ mg_close_connection(struct mg_connection *conn)
 		mg_free(client_ctx);
 		(void)pthread_mutex_destroy(&conn->mutex);
 		mg_free(conn);
-	} else if (client_ctx->context_type == CONTEXT_HTTP_CLIENT) {
+	} else if (conn->phys_ctx->context_type == CONTEXT_HTTP_CLIENT) {
 		mg_free(conn);
 	}
 #else
