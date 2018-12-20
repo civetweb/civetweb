c3a7b2d3f12c6dd0f2d69d02ac9f731fbd3159ff
Uilian Ries <uilianries@gmail.com>
2018-12-20 15:02:38 -0200

    #712 Fix run environment
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/test_package/conanfile.py
+++ conan/test_package/conanfile.py
@@ -19,8 +19,11 @@ class TestPackageConan(ConanFile):
     def test(self):
         assert os.path.isfile(os.path.join(self.deps_cpp_info["civetweb"].rootpath, "licenses", "LICENSE.md"))
         bin_path = os.path.join(os.getcwd(), "bin", "test_package")
-        with tools.environment_append(RunEnvironment(self).vars):
-            process = subprocess.Popen([bin_path], shell=True)
+        run_vars = RunEnvironment(self).vars
+        with tools.environment_append(run_vars):
+            if self.settings.os == "Macos":
+                run_vars["DYLD_LIBRARY_PATH"] = os.environ.get('DYLD_LIBRARY_PATH', '')
+            process = subprocess.Popen([bin_path], shell=True, env=run_vars)
             time.sleep(2)
             response = requests.get("http://localhost:8080/example")
             assert response.ok
