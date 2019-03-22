defd4307ed895dec07353f380e6735818d7f7ca9
Breno Ramalho Lemes <breno@br-lemes.net>
2019-03-22 19:47:44 -0400

    Add includes path

--- resources/jni/Android.mk
+++ resources/jni/Android.mk
@@ -3,4 +3,5 @@ include $(CLEAR_VARS)
 LOCAL_CFLAGS    := -std=c99 -O2 -W -Wall -pthread -pipe $(COPT)
 LOCAL_MODULE    := civetweb
 LOCAL_SRC_FILES := src/main.c src/civetweb.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 include $(BUILD_EXECUTABLE)
